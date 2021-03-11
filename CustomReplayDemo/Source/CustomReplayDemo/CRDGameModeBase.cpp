85bdab2954653597352d22419133a070c1925a74
Nick Prühs <dev@npruehs.de>
2021-03-11 23:48:38 +0100

    Update game world from replay data.

--- CustomReplayDemo/Source/CustomReplayDemo/CRDGameModeBase.cpp
+++ CustomReplayDemo/Source/CustomReplayDemo/CRDGameModeBase.cpp
@@ -2,8 +2,12 @@
 
 #include "JsonObjectConverter.h"
 
-#include "CRDCustomReplay.h"
 #include "CRDLog.h"
+
+ACRDGameModeBase::ACRDGameModeBase()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
 
 void ACRDGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
 {
@@ -17,8 +21,6 @@ void ACRDGameModeBase::InitGame(const FString& MapName, const FString& Options, 
     // Parse JSON.
     FString ReplayJsonString;
     FFileHelper::LoadFileToString(ReplayJsonString, *ReplayPath);
-
-    FCRDCustomReplay Replay;
     FJsonObjectConverter::JsonObjectStringToUStruct<FCRDCustomReplay>(ReplayJsonString, &Replay, 0, 0);
 
     UE_LOG(LogCRD, Log, TEXT("Successfully parsed custom replay file %s"), *ReplayPath);
@@ -33,5 +35,55 @@ void ACRDGameModeBase::InitGame(const FString& MapName, const FString& Options, 
             UE_LOG(LogCRD, Log, TEXT("Frame %i - Unit %s - Location: %f, %f, %f"),
                 FrameIndex, *Unit.Id.ToString(), Unit.LocationX, Unit.LocationY, Unit.LocationZ);
         }
+    }
+
+    // Start playing.
+    ReplayFrame = -1;
+}
+
+void ACRDGameModeBase::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    // Calculate new replay frame index.
+    int32 NewReplayFrame = FMath::FloorToInt(GetWorld()->GetTimeSeconds() / SecondsPerReplayFrame);
+
+    // Check if we need to update the game world.
+    if (NewReplayFrame > ReplayFrame && Replay.Frames.IsValidIndex(NewReplayFrame))
+    {
+        ApplyReplayFrame(NewReplayFrame);
+    }
+}
+
+void ACRDGameModeBase::ApplyReplayFrame(int32 NewReplayFrame)
+{
+    ReplayFrame = NewReplayFrame;
+
+    UE_LOG(LogCRD, Log, TEXT("Replay frame %i started!"), ReplayFrame);
+
+    const FCRDCustomReplayFrame& Frame = Replay.Frames[ReplayFrame];
+
+    // Update all units.
+    for (const FCRDCustomReplayUnitState& ReplayUnit : Frame.Units)
+    {
+        // Get Unreal actor representing the replay unit.
+        AActor* Unit = Units.FindRef(ReplayUnit.Id);
+
+        if (!IsValid(Unit))
+        {
+            // We've never seen that unit before.
+            // Pragmatic approach: Just lazily spawn new actor for the unit.
+            Unit = GetWorld()->SpawnActor(UnitActorClass);
+            Units.Add(ReplayUnit.Id, Unit);
+
+            UE_LOG(LogCRD, Log, TEXT("Spawned new actor %s for unit %s."), *Unit->GetName(), *ReplayUnit.Id.ToString());
+        }
+
+        // Update actor location.
+        FVector NewLocation = FVector(ReplayUnit.LocationX, ReplayUnit.LocationY, ReplayUnit.LocationZ);
+        Unit->SetActorLocation(NewLocation);
+
+        UE_LOG(LogCRD, Log, TEXT("Unit %s moved to location %f, %f, %f"),
+            *ReplayUnit.Id.ToString(), ReplayUnit.LocationX, ReplayUnit.LocationY, ReplayUnit.LocationZ);
     }
 }
--- CustomReplayDemo/Source/CustomReplayDemo/CRDGameModeBase.h
+++ CustomReplayDemo/Source/CustomReplayDemo/CRDGameModeBase.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include "GameFramework/GameModeBase.h"
+
+#include "CRDCustomReplay.h"
+
 #include "CRDGameModeBase.generated.h"
 
 UCLASS()
@@ -10,5 +14,30 @@ class CUSTOMREPLAYDEMO_API ACRDGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 
 public:
+    ACRDGameModeBase();
+
     virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+    virtual void Tick(float DeltaSeconds) override;
+
+private:
+    /** Current replay we're playing. */
+    UPROPERTY()
+    FCRDCustomReplay Replay;
+    
+    /** Current frame of the replay we're playing. */
+    int32 ReplayFrame;
+
+    /** Unreal actors we've spawned for units of the replay. */
+    UPROPERTY()
+    TMap<FName, AActor*> Units;
+
+    /** Duration of a replay frame, in seconds. */
+    UPROPERTY(EditDefaultsOnly)
+    float SecondsPerReplayFrame;
+
+    /** Class of actors to spawn for replay units. */
+    UPROPERTY(EditDefaultsOnly)
+    TSubclassOf<AActor> UnitActorClass;
+
+    void ApplyReplayFrame(int32 NewReplayFrame);
 };
